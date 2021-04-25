edb73ab440b79d29833d17e6d881de7bc96c7fa5
Seth <sethvance26@gmail.com>
2021-04-24 20:24:16 -0400

    Adding in switch cases for license badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+let licenseBadge;
+const renderLicenseBadge = (license) => {
+  
+  switch (license) {
+    case 'MIT':
+      licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'Mozilla':
+      licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      break; 
+    case 'Apache 2.0':
+      licenseBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'None':
+      licenseBadge = '';
+      break;
+    }
+    return licenseBadge;
+}
 
+ 
+  
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+let licenseURL;
+function renderLicenseLink = (license) => {
+  switch (license) {
+    case 'MIT':
+      licenseURL = 'https://opensource.org/licenses/MIT';
+
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
+
+
+
+
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (userInput) => {
 
   const { projectname, email, github, description, usage, licensetype, test, install, contributions, repo } = userInput;
 
+  const licenseBadge = renderLicenseBadge(userInput.licenseType);
+  const licenseURL = renderLicenseLink(userInput.licenseType);
 
   return`
 
